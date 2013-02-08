01d9492f720bfbceaa922a7551e7d592cad8f023
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-08 12:42:48 -0500

    Updated Readme

--- Multiples-3-5.py
+++
@@ -1,7 +0,0 @@
-naturalNumbers = []
-
-for questionNumber in range (1, 100):
-    if(((questionNumber % 5) == 0) or ((questionNumber % 3) == 0)):
-        naturalNumbers.append(questionNumber)
-        
-print naturalNumbers
